9e0e796c2696b53e59af83015fd0feba4186b28d
Danila <divanov1253@gmail.com>
2019-07-30 21:58:41 +0400

    Update C1.c

--- Hello world/01-basics/C1.c
+++ Hello world/01-basics/C1.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
-double zd (double x1,double x2,double y1, double y2,double z1,double z2){
-    double d=sqrt(pow((x2-x1),2)+pow((y2-y1),2)+pow((z2-z1),2));
+double distPoints(double x1, double x2, double y1, double y2, double z1, double z2) {
+    double d = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2) + pow((z2 - z1), 2));
     return d;
 }
-int main(int argc,const char* argv[]){
-    double x1,x2,y1,y2,z1,z2;
-    if(argc<7){
-        double ot=zd(x1,x2,y1,y2,z1,z2);
-        printf("%lf", ot);
-    } else{
+
+int main(int argc, const char* argv[]) {
+    double x1, x2, y1, y2, z1, z2;
+    x1=atof(argv[1]);
+    y1=atof(argv[2]);
+    z1=atof(argv[3]);
+    x2=atof(argv[4]);
+    y2=atof(argv[5]);
+    z2=atof(argv[6]);
+    if (argc != 7) {
         printf("Введите координаты точек: x1,y1,z1,x2,y2,z2");
-        scanf("%lf %lf %lf %lf %lf %lf",&x1,&y1,&z1,&x2,&y2,&z2);
-        double ot=zd(x1,x2,y1,y2,z1,z2);
-        printf("%lf", ot);
+        scanf("%lf %lf %lf %lf %lf %lf", &x1, &y1, &z1, &x2, &y2, &z2);
+
     }
+    double ot = distPoints(x1, x2, y1, y2, z1, z2);
+    printf("%lf", ot);
 }
